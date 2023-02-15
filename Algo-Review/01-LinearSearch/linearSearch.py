06de7288a602f2766f74f3314ea450eabea17f5e
ykimdeveloper <ykimdeveloper@gmail.com>
2023-02-14 22:04:13 -0800

    adding linearsearch for python

---
+++ Algo-Review/01-LinearSearch/linearSearch.py
@@ -0,0 +1,18 @@
+def linearSearch(arr, val):
+    for i in range(0, len(arr)):
+        if arr[i] == val:
+            return i 
+    return -1
+
+def main():
+    arr = [53, 13, 64, 34, 12, 63, 91]
+    val = 34
+    index = linearSearch(arr, val)
+    if (index != -1):
+        print('Element found at index ', index)
+    else:
+        print('Element not found ')
+     
+
+if __name__ == "__main__":
+    main()

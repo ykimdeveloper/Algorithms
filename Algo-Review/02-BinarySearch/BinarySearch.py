f86bf3ad86f011ca50777cb83914c0a552681582
ykimdeveloper <ykimdeveloper@gmail.com>
2023-02-17 18:10:30 -0800

    modified binary search for python

--- Algo-Review/02-BinarySearch/BinarySearch.py
+++ Algo-Review/02-BinarySearch/BinarySearch.py
@@ -1,25 +1,21 @@
 def binary_search(array, target):
-    start = 0
-    end = len(array) - 1
-    while (start <= end):
-        middle = start + int((end-start)/2)
-        val = array[middle]
-        if(middle == val):
-            return middle
-        elif(val<target):
-            start = middle + 1
-        elif( val > target):
-            end = middle - 1
-        else:
-            return middle
-
-
-    return -1 
+	start = 0
+	end = len(array)-1 
+	while(start <=end):
+		middle = start + int((end-start)/2)
+		if(array[middle] == target):
+			return middle 
+		elif(array[middle] < target):
+			start = middle + 1
+		elif(array[middle] > target):
+			end = middle -1
+		else:
+			return -1
 
 
 def main():
-    array = [13, 24, 30, 35, 44, 48, 59, 67, 71, 82, 89, 91, 108, 159, 201]
-    target = 91
+    array = [13, 24, 30, 35, 44, 48, 59, 67, 71, 82, 89, 91, 108, 159, 201, 202]
+    target = 108
     index = binary_search(array, target)
     if (index == -1):
         print("Element does not exist")
